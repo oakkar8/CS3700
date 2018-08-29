594247b2ca4974279ed05a5bde3bc3f56d1bbf20
Oakar Kyaw <oakkar8@gmail.com>
2018-08-29 14:49:16 -0700

    last modifed

--- HW1/hwOne.java
+++ HW1/hwOne.java
@@ -1,5 +1,5 @@
 public class hwOne{
     public static void main(String[] args){
-        System.out.println("Hello, my name is Oakar Kyaw");
+        System.out.println("Goodbye, I finished my CS 3700 HW1");
     }
 }
